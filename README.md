9db2bbe87dc17279784604c4fcd6ff28e924320e
lrusc <ruschioni.l@gmail.com>
2023-01-08 12:22:34 -0300

    poo_2022_projeto_django

--- README.md
+++
@@ -1,3 +0,0 @@
-# Primeiro projeto com o framework Django
-Projeto para conhecer e explorar o framework Django realizado durante a Unidade Curricular: Programação Orientada a Objetos (Programação II) 2022
-do curso Tecnologia em Informática em Saúde da Universidade Federal de São Paulo.
--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
